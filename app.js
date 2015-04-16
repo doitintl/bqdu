7996a7306cae427d533ae95e8d3d1b12fbf89306
Spark2Ignite <vadim@doit-intl.com>
2015-04-17 01:48:12 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,223 @@
+/**
+ * Created by vadim on 16/4/15.
+ */
+
+// Google Analytics
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-39701609-5', 'auto');
+ga('send', 'pageview');
+
+google.load("visualization", "1", {packages: ["treemap"]});
+
+function showFullTooltip(row, size, value) {
+    return '<div style="background:#fd9; padding:10px; border-style:solid; border-color:white">' + 'Size: ' + numberWithCommas(Math.round(chartData.getValue(row, 2))) + ' GB' + '<br> Row: ' + numberWithCommas(Math.round(chartData.getValue(row, 3))) + 'M' + '</div>';
+}
+
+// number formatting
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// User Submitted Variables
+var project_id = 'doit-intl.com:ezrakhovichg2';
+var client_id = '968052747331-6rhnplku61s0aokfjdsil04v7c5clck1.apps.googleusercontent.com';
+
+var config = {
+    'client_id': client_id,
+    'scope': 'https://www.googleapis.com/auth/bigquery'
+};
+
+function showProjects() {
+    var request = gapi.client.bigquery.projects.list();
+    request.execute(function (response) {
+        $('#result_box').html(JSON.stringify(response, null));
+    });
+}
+
+function getTableData(projectId, success, failed) {
+    var projectId = "glowing-indexer-494";
+    var data = [['table', 'dataset', 'size', 'rows'], [projectId, null, 0, 0]];
+    var index = 2;
+    var dataset_index = 0;
+    var dsSize = 0;
+    var dsRows = 0;
+    var totalSize = 0; // total data in project
+    var totalRows = 0; // total rows in project
+    var ntables = 0; // total number of tables in project
+    var datasets;
+    var tableRes = 0;
+    var tableCalls = 0;
+
+    function onGetTableInfo(response){
+        tableRes++;
+        var dataset = this;
+        var tableRows = parseInt(response.numRows) / 1000000; // get number of rows in millions
+        var tableSize = parseInt(response.numBytes) / 1024 / 1024 / 1024; // get size of the table in GB
+        if (isNaN(tableSize)) {
+            tableSize = 0;
+        }
+
+        data[index] = [{
+            v: response.result.id,
+            f: response.tableReference.tableId
+        }, dataset.datasetReference.datasetId, parseInt(response.numBytes), parseInt(response.numRows)];
+        index++;
+
+        dsSize = dsSize + tableSize;
+        dsRows = dsRows + tableRows;
+
+        totalSize = totalSize + tableSize; // calculate total size of all tables in all datasets
+        totalRows = totalRows + tableRows; // calculate total rows of all tables in all datasets
+        if (tableCalls == tableRes) {success(data)}
+    }
+
+    function onGetTables(response) {
+        var dataset = this;
+        var tables = response.tables;
+
+        // if tables exist in dataset, get tableSize & tableRows from each table
+        if (tables) {
+            dataset_index++;
+
+            tables.forEach(function (table) {
+                tableCalls++;
+                current_table = table;
+                var request = gapi.client.bigquery.tables.get({
+                    projectId: projectId,
+                    datasetId: dataset.datasetReference.datasetId,
+                    tableId: table.tableReference.tableId
+                });
+                request.execute(onGetTableInfo.bind(dataset));
+            });
+                data[index] = [dataset.datasetReference.datasetId, projectId, dsSize, dsRows];
+                index++;
+
+            dsSize = 0;
+        }
+    }
+
+    function onGetDatasets(response) {
+        var datasets = response.datasets;
+        if (!datasets) return;
+
+        // Fetching all tables from each dataset
+        datasets.forEach(function (dataset) {
+            if (!dataset) return;
+            //current_dataset = dataset;
+            var request = gapi.client.bigquery.tables.list({
+                projectId: projectId, datasetId: dataset.datasetReference.datasetId, maxResults: 100
+            });
+
+            request.execute(onGetTables.bind(dataset));
+
+            //ntables = ntables + tables.tables.length; // not sure I still need it
+
+        });
+    }
+
+    // Fetching all datasets for given project
+    var request = gapi.client.bigquery.datasets.list({projectId: projectId}, {maxResults: 100});
+    request.execute(onGetDatasets);
+}
+
+function auth() {
+    gapi.auth.authorize(config, function () {
+        gapi.client.load('bigquery', 'v2');
+        $('#client_initiated').html('BigQuery client initiated');
+        $('#auth_button').fadeOut();
+        $('#projects_button').fadeIn();
+        $('#dataset_button').fadeIn();
+        $('#table_info_button').fadeIn();
+    });
+}
+
+var app = angular.module('app', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdDialog', '$timeout', function ($scope, $mdSidenav, $mdDialog, $timeout) {
+    //$scope.$apply(function()
+    //  {
+    $scope.timeoutMsg = "Fetching your projects...just a sec...";
+    $scope.projectId = "plexop-gcp-01";
+    //  })
+
+    //google.script.run.withSuccessHandler(getProjectsSuccess).withFailureHandler(getProjectsFailed).getProjects();
+
+    function getProjectsSuccess(projectList) {
+        $scope.$apply(function () {
+            $scope.timeoutMsg = "Select Project ID";
+            $scope.projectList = projectList;
+            debugger;
+        });
+    }
+
+    function getProjectsFailed() {
+    }
+
+    $scope.toggleSidenav = function (menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+
+    $scope.analyze = function () {
+
+        $scope.showProgress = true;
+
+        getTableData($scope.projectId, drawChart, callbackfailed);
+
+        function callbackfailed(e) {
+            $scope.showProgress = false;
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.body))
+                    .title('Aarrgh! Something went wrong...')
+                    .content('There was an error fetching your BigQuery datasets. Make sure you are using correct ProjectID or maybe just bad luck')
+                    .ok('OK. Let me try again!')
+            );
+            $scope.projectId = "";
+        }
+
+        function drawChart(data) {
+            console.log('draw chart: %O', data);
+            window.rowdata = data;
+            $timeout(function () {
+                $scope.showProgress = false;
+            });
+
+            chartData = google.visualization.arrayToDataTable(data);
+            tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
+
+            tree.draw(chartData, {
+                minColor: '#BBDEFB',
+                midColor: '#42A5F5',
+                maxColor: '#1976D2',
+                headerHeight: 15,
+                fontColor: 'white',
+                showScale: false,
+                maxDepth: 1,
+                showTooltips: true,
+                generateTooltip: showFullTooltip
+
+            })
+
+        }
+                window.draw = drawChart;
+    };
+
+    function onSuccessEmail(email) {
+        $scope.$apply(function () {
+            $scope.email = email
+        });
+    }
+
+    //google.script.run.withSuccessHandler(onSuccessEmail).getUser();
+
+}]);
---
+++ demo.json.js
@@ -0,0 +1,374 @@
+[[table, dataset, size, rows], [plexop - gcp - 01, null, 0.0, 0.0], [{f=imps_20150130, v=plexop - gcp - 01
+:
+agency_4.imps_20150130
+},
+agency_4, 9.831704637035728, 20.039791
+],
+[{f=imps_20150131, v=plexop - gcp - 01
+:
+agency_4.imps_20150131
+},
+agency_4, 5.0060054156929255, 10.114469
+],
+[{f=imps_20150201, v=plexop - gcp - 01
+:
+agency_4.imps_20150201
+},
+agency_4, 4.703638904727995, 10.71723
+],
+[{f=imps_20150202, v=plexop - gcp - 01
+:
+agency_4.imps_20150202
+},
+agency_4, 7.923207669518888, 16.815422
+],
+[{f=imps_20150203, v=plexop - gcp - 01
+:
+agency_4.imps_20150203
+},
+agency_4, 11.152173588052392, 22.277196
+],
+[{f=imps_20150204, v=plexop - gcp - 01
+:
+agency_4.imps_20150204
+},
+agency_4, 17.677850296720862, 32.230008
+],
+[{f=imps_20150205, v=plexop - gcp - 01
+:
+agency_4.imps_20150205
+},
+agency_4, 17.46435530204326, 31.989202
+],
+[{f=imps_20150206, v=plexop - gcp - 01
+:
+agency_4.imps_20150206
+},
+agency_4, 13.410089200362563, 25.556151
+],
+[{f=imps_20150207, v=plexop - gcp - 01
+:
+agency_4.imps_20150207
+},
+agency_4, 12.001298044808209, 21.599935
+],
+[{f=imps_20150208, v=plexop - gcp - 01
+:
+agency_4.imps_20150208
+},
+agency_4, 7.901731128804386, 15.806901
+],
+[{f=imps_20150209, v=plexop - gcp - 01
+:
+agency_4.imps_20150209
+},
+agency_4, 10.057977931573987, 20.315337
+],
+[{f=imps_20150210, v=plexop - gcp - 01
+:
+agency_4.imps_20150210
+},
+agency_4, 10.454701072536409, 20.229673
+],
+[{f=imps_20150211, v=plexop - gcp - 01
+:
+agency_4.imps_20150211
+},
+agency_4, 10.343585244379938, 20.709546
+],
+[{f=imps_20150212, v=plexop - gcp - 01
+:
+agency_4.imps_20150212
+},
+agency_4, 8.871296158991754, 16.367024
+],
+[{f=imps_20150213, v=plexop - gcp - 01
+:
+agency_4.imps_20150213
+},
+agency_4, 8.715050143189728, 16.709838
+],
+[{f=imps_20150214, v=plexop - gcp - 01
+:
+agency_4.imps_20150214
+},
+agency_4, 8.282304841093719, 15.309947
+],
+[{f=imps_20150215, v=plexop - gcp - 01
+:
+agency_4.imps_20150215
+},
+agency_4, 9.67190876044333, 19.323233
+],
+[{f=imps_20150216, v=plexop - gcp - 01
+:
+agency_4.imps_20150216
+},
+agency_4, 12.804898244328797, 27.23665
+],
+[{f=imps_20150217, v=plexop - gcp - 01
+:
+agency_4.imps_20150217
+},
+agency_4, 9.927178738638759, 19.698615
+],
+[{f=imps_20150218, v=plexop - gcp - 01
+:
+agency_4.imps_20150218
+},
+agency_4, 9.70984878949821, 18.769088
+],
+[{f=imps_20150219, v=plexop - gcp - 01
+:
+agency_4.imps_20150219
+},
+agency_4, 9.490345122292638, 18.237174
+],
+[{f=imps_20150220, v=plexop - gcp - 01
+:
+agency_4.imps_20150220
+},
+agency_4, 8.45170721784234, 16.497751
+],
+[{f=imps_20150221, v=plexop - gcp - 01
+:
+agency_4.imps_20150221
+},
+agency_4, 7.466877928934991, 13.633896
+],
+[{f=imps_20150222, v=plexop - gcp - 01
+:
+agency_4.imps_20150222
+},
+agency_4, 7.23573971260339, 12.973658
+],
+[{f=imps_20150223, v=plexop - gcp - 01
+:
+agency_4.imps_20150223
+},
+agency_4, 11.529259410686791, 22.333924
+],
+[{f=imps_20150224, v=plexop - gcp - 01
+:
+agency_4.imps_20150224
+},
+agency_4, 12.93134918436408, 24.851177
+],
+[{f=imps_20150225, v=plexop - gcp - 01
+:
+agency_4.imps_20150225
+},
+agency_4, 12.976403445005417, 24.498819
+],
+[{f=imps_20150226, v=plexop - gcp - 01
+:
+agency_4.imps_20150226
+},
+agency_4, 9.288880252279341, 16.277361
+],
+[{f=imps_20150227, v=plexop - gcp - 01
+:
+agency_4.imps_20150227
+},
+agency_4, 8.677835959941149, 14.493693
+],
+[{f=imps_20150228, v=plexop - gcp - 01
+:
+agency_4.imps_20150228
+},
+agency_4, 7.748415371403098, 12.98354
+],
+[{f=imps_20150301, v=plexop - gcp - 01
+:
+agency_4.imps_20150301
+},
+agency_4, 8.607117298990488, 15.308167
+],
+[{f=imps_20150302, v=plexop - gcp - 01
+:
+agency_4.imps_20150302
+},
+agency_4, 12.813103654421866, 23.953608
+],
+[{f=imps_20150303, v=plexop - gcp - 01
+:
+agency_4.imps_20150303
+},
+agency_4, 11.752140382304788, 20.833028
+],
+[{f=imps_20150304, v=plexop - gcp - 01
+:
+agency_4.imps_20150304
+},
+agency_4, 14.33013394009322, 27.230526
+],
+[{f=imps_20150305, v=plexop - gcp - 01
+:
+agency_4.imps_20150305
+},
+agency_4, 14.794570477679372, 26.722624
+],
+[{f=imps_20150306, v=plexop - gcp - 01
+:
+agency_4.imps_20150306
+},
+agency_4, 11.504068397916853, 21.3335
+],
+[{f=imps_20150307, v=plexop - gcp - 01
+:
+agency_4.imps_20150307
+},
+agency_4, 12.860119627788663, 22.522024
+],
+[{f=imps_20150308, v=plexop - gcp - 01
+:
+agency_4.imps_20150308
+},
+agency_4, 8.89952680375427, 18.921629
+],
+[{f=imps_20150309, v=plexop - gcp - 01
+:
+agency_4.imps_20150309
+},
+agency_4, 15.779861158691347, 31.916363
+],
+[{f=imps_20150310, v=plexop - gcp - 01
+:
+agency_4.imps_20150310
+},
+agency_4, 15.626889714971185, 30.317322
+],
+[{f=imps_20150311, v=plexop - gcp - 01
+:
+agency_4.imps_20150311
+},
+agency_4, 13.237907711416483, 26.219263
+],
+[{f=imps_20150312, v=plexop - gcp - 01
+:
+agency_4.imps_20150312
+},
+agency_4, 11.62958843074739, 23.518525
+],
+[{f=imps_20150313, v=plexop - gcp - 01
+:
+agency_4.imps_20150313
+},
+agency_4, 13.20411703363061, 24.606554
+],
+[{f=imps_20150314, v=plexop - gcp - 01
+:
+agency_4.imps_20150314
+},
+agency_4, 11.055767135694623, 20.200739
+],
+[{f=imps_20150315, v=plexop - gcp - 01
+:
+agency_4.imps_20150315
+},
+agency_4, 10.90174851194024, 20.005379
+],
+[{f=imps_20150316, v=plexop - gcp - 01
+:
+agency_4.imps_20150316
+},
+agency_4, 11.80820573400706, 21.043183
+],
+[{f=imps_20150317, v=plexop - gcp - 01
+:
+agency_4.imps_20150317
+},
+agency_4, 11.635988636873662, 20.854444
+],
+[{f=imps_20150318, v=plexop - gcp - 01
+:
+agency_4.imps_20150318
+},
+agency_4, 11.988803178071976, 22.474613
+],
+[{f=imps_20150319, v=plexop - gcp - 01
+:
+agency_4.imps_20150319
+},
+agency_4, 11.128746250644326, 21.168928
+],
+[{f=imps_20150320, v=plexop - gcp - 01
+:
+agency_4.imps_20150320
+},
+agency_4, 8.092421509325504, 15.858118
+],
+[{f=imps_20150321, v=plexop - gcp - 01
+:
+agency_4.imps_20150321
+},
+agency_4, 9.537581561133265, 16.798506
+],
+[{f=imps_20150322, v=plexop - gcp - 01
+:
+agency_4.imps_20150322
+},
+agency_4, 10.227861768566072, 18.558422
+],
+[{f=imps_20150323, v=plexop - gcp - 01
+:
+agency_4.imps_20150323
+},
+agency_4, 13.976057056337595, 24.842615
+],
+[{f=imps_20150324, v=plexop - gcp - 01
+:
+agency_4.imps_20150324
+},
+agency_4, 13.644686976447701, 23.964176
+],
+[{f=imps_20150325, v=plexop - gcp - 01
+:
+agency_4.imps_20150325
+},
+agency_4, 13.876289918087423, 23.654303
+],
+[{f=imps_20150326, v=plexop - gcp - 01
+:
+agency_4.imps_20150326
+},
+agency_4, 12.878220438957214, 23.105063
+],
+[{f=imps_20150327, v=plexop - gcp - 01
+:
+agency_4.imps_20150327
+},
+agency_4, 11.039068686775863, 20.117342
+],
+[{f=imps_20150328, v=plexop - gcp - 01
+:
+agency_4.imps_20150328
+},
+agency_4, 9.582150298170745, 16.862555
+],
+[{f=imps_20150329, v=plexop - gcp - 01
+:
+agency_4.imps_20150329
+},
+agency_4, 9.691328252665699, 17.184382
+],
+[{f=imps_20150330, v=plexop - gcp - 01
+:
+agency_4.imps_20150330
+},
+agency_4, 12.122493166476488, 20.397132
+],
+[{f=only_simon, v=plexop - gcp - 01
+:
+agency_4.only_simon
+},
+agency_4, 0.008554202504456043, 0.026288
+],
+[{f=simon, v=plexop - gcp - 01
+:
+agency_4.simon
+},
+agency_4, 0.27426670864224434, 0.735974
+],
+[agency_4, plexop - gcp - 01, 660.2170023415238, 1239.851544]
+]/**
