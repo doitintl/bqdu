e676f153ab6648acabf3519f7ab4cbc71d220e99
Vadim Solovey <vadim@budgetao.com>
2015-04-21 08:44:51 +0300

    removed clean task from grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,20 +24,10 @@ module.exports = function(grunt) {
 				'footer': ''
 			}
 		},
-		clean: {
-			task: {
-				src: ['app.js'],
-				dest: 'dist/app.js'
-			},
-			options: {
-				'force': false,
-				'no-write': false
-			}
-		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('default', ['uglify', 'clean']);
+	grunt.registerTask('default', ['uglify']);
 };
